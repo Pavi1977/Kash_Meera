d68d3652325572256a85861fc25ad189c753e52d
Pavi1977 <159613554+Pavi1977@users.noreply.github.com>
2024-02-10 15:38:39 +0530

    Update helloworld.java
    update

--- helloworld.java
+++ helloworld.java
@@ -6,7 +6,7 @@ public class helloworld
 		int a=100;
 		int b=75;
 		int d = a - b;
-		int e = a*b:
+		int e = a*b;
 		int f = a / b;			
 		System.out.println("Subtraction of a and b"+d);
 		System.out.println("Multiplication of a and b"+e);
