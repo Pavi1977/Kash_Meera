0140a6e8a399f79032491de27f996eece450c974
Pavi1977 <159613554+Pavi1977@users.noreply.github.com>
2024-02-10 15:01:50 +0530

    Update helloworld.java
    Subtraction of A and B

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,10 @@ public class helloworld
 	public static void main (String args[])
 	{
 		System.out.println("Hello World! Welcome to DevOps Training Section!!!!");
+		int a=100;
+		int b=75;
+		int d = a - b;
+		System.out.println("Subtraction of a and b"+d);
 	}
 
 }
