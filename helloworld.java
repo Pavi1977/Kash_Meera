2926d611817c857da79e73c5b1c62ae5dc0bb551
Pavi1977 <159613554+Pavi1977@users.noreply.github.com>
2024-02-10 15:13:34 +0530

    Update helloworld.java
    Multiplication

--- helloworld.java
+++ helloworld.java
@@ -6,7 +6,9 @@ public class helloworld
 		int a=100;
 		int b=75;
 		int d = a - b;
+		int e = a*b:
 		System.out.println("Subtraction of a and b"+d);
+		System.out.println("Multiplication of a and b"+e);
 	}
 
 }
