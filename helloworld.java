a853bcb5cf7c0b2e1c02c320f8a6c2956526a92a
Pavi1977 <159613554+Pavi1977@users.noreply.github.com>
2024-02-13 15:16:52 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -27,7 +27,7 @@ public class helloworld
 		}
 	System.out.println("Fibonacci Series");
 		int n1=0, int n2=1, n3,i, count=10;
-		System.out.println(n1+" ");
+		System.out.println(n1);
 		for(i=2;i<count;++i)
 		    	{
 				n3=n1+n2;
