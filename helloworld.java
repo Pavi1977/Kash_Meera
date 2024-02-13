45b3414a72262d7b6d5d51371e703ce4e2d93b78
Pavi1977 <159613554+Pavi1977@users.noreply.github.com>
2024-02-13 15:04:00 +0530

    Update helloworld.java
    Semi-colon issue

--- helloworld.java
+++ helloworld.java
@@ -28,7 +28,7 @@ public class helloworld
 	System.out.println("Fibonacci Series");
 		int n1=0, int n2=1, n3,i, count=10;
 		System.out.println(n1+" "+n2);
-		for(i=2,i<count,++i)
+		for(i=2;i<count;++i)
 		    	{
 				n3=n2;
 				System.out.println(" "+n3);
